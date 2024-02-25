3e0b344368bce2186dc761d1456560db8cf3a8a2
Valenti25 <161212070+Valenti25@users.noreply.github.com>
2024-02-25 22:56:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const progressEL = document.querySelector(".progress");
+const openBtn=document.querySelector(".open-btn")
+const closeBtn=document.querySelector(".close-btn")
+const modalContainer=document.querySelector(".modal-container")
+
+
+openBtn.addEventListener("click",()=>{
+    modalContainer.classList.add("show")
+})
+closeBtn.addEventListener("click",()=>{
+    modalContainer.classList.remove("show")
+})
+
 
 window.onscroll = () => scrollProgress();
 
